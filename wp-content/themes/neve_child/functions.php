b954fafd420ba26c3fd52fc74d191ec23735a1e2
PaulineSou <pauline.soudry@gmail.com>
2023-03-15 17:47:49 +0100

    modif responsive

--- wp-content/themes/neve_child/functions.php
+++ wp-content/themes/neve_child/functions.php
@@ -19,7 +19,7 @@ add_filter( 'wp_nav_menu_items','add_admin_link', 10, 2 );
 
 function add_admin_link( $items, $args ) {
     if (is_user_logged_in() && $args->theme_location == 'primary') {
-        $items .= '<li><a href="'. get_admin_url() .'">Admin</a></li>';
+        $items .= '<li class="admin"><a href="'. get_admin_url() .'">Admin</a></li>';
     }
     return $items;
 }
