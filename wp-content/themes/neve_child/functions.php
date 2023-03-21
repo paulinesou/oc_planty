f0176508a525fbaa189399e0fef97446dc54fb05
PaulineSou <pauline.soudry@gmail.com>
2023-03-21 11:02:26 +0100

    correction orthographe

--- wp-content/themes/neve_child/functions.php
+++ wp-content/themes/neve_child/functions.php
@@ -5,7 +5,7 @@ add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles(){
     // Chargement du style.css du thème parent Neve
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-    // Chargement du themeChild.css thèeme personnalisé
+    // Chargement du themeChild.css thème personnalisé
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/assets/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/assets/css/theme.css'));
 }
 ?>
